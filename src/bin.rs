c2ce59c5c74703df3169afbafc31366b765eabd0
mvonlanthen <marcel@t440s-marcel.localdomain>
2020-10-10 20:58:39 +0200

    bugfix

--- src/bin.rs
+++ src/bin.rs
@@ -1,8 +1,10 @@
 use ndarray::prelude::*;
 // use ndarray;
+// use inpolygon::pt_in_polygon;
 use inpolygon::pts_in_polygon;
-// use ndarray_rand::RandomExt;
-// use ndarray_rand::rand_distr::Uniform;
+use ndarray_rand::RandomExt;
+use ndarray_rand::rand_distr::Uniform;
+use std::time::Instant;
 
 
 // fn test_func(a1: &Array2<f64>, a2: &ArrayBase<ndarray::ViewRepr<&f64>, Dim<[usize; 2]>>) {
@@ -10,42 +12,46 @@ use inpolygon::pts_in_polygon;
 // }
 
 pub fn main() {
-    let polygon  = array![
-        [-1., -1.],
-        [ 1., -1.],
-        [ 1.,  1.],
-        [-1.,  1.],
-        [-1., -1.]
+    let polygon = array![
+        [-1.0,-1.0],
+        [1.0,-1.0],
+        [1.0,1.0],
+        [-0.5,1.0],
+        [-1.0,0.0],
+        [-1.0,-1.0]
     ];
 
-    let points = array![
-        [0., 0.],
-        [2., 0.],
-        [1., 0.],
-        [0., -1.]
-    ];
+    // let points = array![
+    //     [0., 0.],
+    //     [2., 0.],
+    //     [1., 0.],
+    //     [0., -1.]
+    // ];
 
-    // let points = Array::random(
-    //     (1000, 2), 
-    //     Uniform::new(-2., 2.)
-    // );
+    // generate a ton of points for benchmark!
+    let nb_points = 10_000_000;
+    let points = Array::random((nb_points, 2), Uniform::new(-2., 2.));
 
-    // this:
-    // let mut is_inside = Array::from_elem(points.len_of(Axis(0)), false);
-    // for (i,point) in points.axis_iter(Axis(0)).enumerate() {
-    //     is_inside[i] = pt_in_polygon(&point, &polygon.view(), true);
-    // }
-    // or that
-    let is_inside = pts_in_polygon(&points.view(), &polygon.view(), true);
-    // finally
-    println!("{}", is_inside);
+    let now = Instant::now();
+    let _is_inside = pts_in_polygon(&points.view(), &polygon.view(), true);
+    let exec_time = now.elapsed().as_micros() as f64;
+    println!("exec time: {}s", exec_time/1000000.0);
+    // // finally
+    // println!("{}", is_inside);
 
-    // let mut is_inside: bool;
-    // for point in points.axis_iter(Axis(0)) {
-    //     is_inside = pt_in_polygon(&point, &polygon.view(), true);
-    //     println!("[{:.02}, {:.02}] is inside: {}", point[0], point[1], is_inside);
-    // }
+    
+    // let polygon = array![
+    //     [-1.0,-1.0],
+    //     [1.0,-1.0],
+    //     [1.0,1.0],
+    //     [-0.5,1.0],
+    //     [-1.0,0.0],
+    //     [-1.0,-1.0]
+    // ];
+    // let point = array![-1.,1.];
+    // println!("point {}", point);
 
-
+    // let is_inside = pt_in_polygon(&point.view(), &polygon.view(), true);
+    // println!("{}", is_inside);
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,26 +12,12 @@ use ndarray::prelude::*;
 /// It works only with 2D points and polygon. The polygon must be close, i.e. the 
 /// last point must be the same than the first point. To maximize execution speed, 
 /// no argument checks are performed (e.g. are the points really 2D?).
-/// 
-/// # Examples
-/// 
-/// ```
-/// use ndarray::prelude::*;
-/// let polygon = array![[-1.,-1.], [1.,-1.], [1.,1.], [-1.,1.],[-1.,-1.]];
-/// let points = array![[0., 0.], [2., 0.], [1., 0.], [0., -1.]];
-/// let is_inside = pts_in_polygon(&points.view(), &polygon.view(), true);
-/// assert!(is_inside == array![true, false, true, true]);
-/// ```
 pub fn pts_in_polygon(
     points: &ArrayView2<f64>,
     polygon: &ArrayView2<f64>, 
     include_edges: bool
 ) -> Array1<bool> {
-    let mut is_inside = Array::from_elem(points.len_of(Axis(0)), false);
-    for (i,point) in points.axis_iter(Axis(0)).enumerate() {
-        is_inside[i] = pt_in_polygon(&point.view(), &polygon.view(), include_edges);
-    }
-    return is_inside;
+    points.map_axis(Axis(1), |pt| pt_in_polygon(&pt, &polygon, include_edges))
 }
 
 /// Compute if a point is inside and/or on the edges of a polygon. Works only with 
@@ -41,11 +27,12 @@ pub fn pt_in_polygon(
     polygon: &ArrayView2<f64>, 
     include_edges: bool
 ) -> bool {
+    // extract some variable
     let x = &point[0];
     let y = &point[1];
     let nb_poly_pts = polygon.len_of(Axis(0));
     let mut counter  = 0;
-    let mut  x_intersect = 0.0;
+    let mut  x_intersect: f64;
 
     // a little trick to handle a point on horizontal edges
     let count_on_horz = if include_edges==true {2} else {1};
@@ -56,26 +43,26 @@ pub fn pt_in_polygon(
     let mut p1y = &polygon[[0,1]];
     let mut p2x: &f64;
     let mut p2y: &f64;
-    for i in 1..nb_poly_pts {
-        p2x = &polygon[[i%nb_poly_pts,0]];
-        p2y = &polygon[[i%nb_poly_pts,1]];
+    for i in 0..nb_poly_pts-1 {
+        p2x = &polygon[[i+1,0]];
+        p2y = &polygon[[i+1,1]];
         if p1y==p2y {
             // test if the point is on horizontal edge
-            if (*y==*p1y) & ((*x>p1x.min(*p2x)) & (*x<p1x.min(*p2x))){
+            if (*y==*p1y) & ((*x>=p1x.min(*p2x)) & (*x<p1x.min(*p2x))){
                 counter += count_on_horz;
             }
         } else { // p1y!= p2y
             // check if the right ray from the point intersect with the current edge
             if (*y>=p1y.min(*p2y)) & (*y<=p1y.max(*p2y)) {
                 x_intersect = (y-p1y) * (p2x-p1x)/(p2y-p1y) + p1x;
-            }
-            // check if the point is right on the edge
-            if (*x==x_intersect) & (include_edges==true) {
-                counter += 1;
-            // check if the point is on the left of the current edge
-            } else if *x<x_intersect {
-                counter += 1;
-            }
+                // check if the point is exactly on the edge
+                if (*x==x_intersect) & (include_edges==true){
+                    counter += 1;
+                // check if the point is on the left of the current edge
+                } else if *x<x_intersect {
+                    counter += 1;
+                }
+            } 
         }
 
         // go the next edge
@@ -110,14 +97,26 @@ mod tests {
     fn test_pts_in_polygon() {
         // a cube of side 2, aligned with the coordinate system and centred on [0,0].
         let polygon = array![
-            [-1.,-1.], [1.,-1.], [1.,1.], [-1.,1.],[-1.,-1.]
+            [-1.0,-1.0],
+            [1.0,-1.0],
+            [1.0,1.0],
+            [-0.5,1.0],
+            [-1.0,0.0],
+            [-1.0,-1.0]
         ];
         let points = array![
-            [0., 0.], [2., 0.], [1., 0.], [0., -1.] 
+            [0., 0.],
+            [0.,2.],
+            [2.,0.],
+            [1.,0.],
+            [0.,1.],
+            [-2.,0.],
+            [-0.75,1.],
+            [-1.,1.]
         ];
 
         let is_inside = pts_in_polygon(&points.view(), &polygon.view(), true);
-        assert!(is_inside == array![true, false, true, true]);
+        assert!(is_inside == array![true, false, false, true, true, true, false, false]);
     } 
 
 }
